d6bb3f34aa689fb4d416141e7594573b984c1dcf
budi <budiboy195@gmail.com>
2024-08-07 09:15:41 +0700

    add d outer dom and change app js route

--- src/App.jsx
+++ src/App.jsx
@@ -20,35 +20,7 @@
 
 // export default App;
 
-// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-// import Home from "../src/pages/home/Home";
-// import About from "../src/pages/about/About";
-// import Navbar from "../src/components/navbar/Navbar";
-// import Footer from "../src/components/footer/Footer";
-// import ScrollToTop from "./dataHelper/ScrollToTop";
-// import './App.css';
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//     <ScrollToTop>
-//       <Navbar />
-//       <Routes>
-//         {/* <Route path="/" element={<Navigate to="/home" />} /> */}
-//         <Route path="/" element={<Home />} />
-//         <Route path="/about" element={<About />} />
-//       </Routes>
-//       <Footer />
-
-//     </ScrollToTop>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-
-
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "../src/pages/home/Home";
 import About from "../src/pages/about/About";
 import Navbar from "../src/components/navbar/Navbar";
@@ -56,28 +28,56 @@ import Footer from "../src/components/footer/Footer";
 import ScrollToTop from "./dataHelper/ScrollToTop";
 import './App.css';
 
-const Layout = () => (
-  <ScrollToTop>
-    <Navbar />
-    <Outlet />
-    <Footer />
-  </ScrollToTop>
-);
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "about", element: <About /> },
-    ],
-  },
-]);
-
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+    <ScrollToTop>
+      <Navbar />
+      <Routes>
+        {/* <Route path="/" element={<Navigate to="/home" />} /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+
+    </ScrollToTop>
+    </BrowserRouter>
+  );
 }
 
 export default App;
+
+
+// import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+// import Home from "../src/pages/home/Home";
+// import About from "../src/pages/about/About";
+// import Navbar from "../src/components/navbar/Navbar";
+// import Footer from "../src/components/footer/Footer";
+// import ScrollToTop from "./dataHelper/ScrollToTop";
+// import './App.css';
+
+// const Layout = () => (
+//   <ScrollToTop>
+//     <Navbar />
+//     <Outlet />
+//     <Footer />
+//   </ScrollToTop>
+// );
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       { index: true, element: <Home /> },
+//       { path: "about", element: <About /> },
+//     ],
+//   },
+// ]);
+
+// function App() {
+//   return <RouterProvider router={router} />;
+// }
+
+// export default App;
 
