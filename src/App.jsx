e58910ccfd69b48149f0104a4fd385e3d8968016
budi <budiboy195@gmail.com>
2024-08-07 08:56:04 +0700

    change routh path and vite config js

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,35 @@
 
 // export default App;
 
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// import Home from "../src/pages/home/Home";
+// import About from "../src/pages/about/About";
+// import Navbar from "../src/components/navbar/Navbar";
+// import Footer from "../src/components/footer/Footer";
+// import ScrollToTop from "./dataHelper/ScrollToTop";
+// import './App.css';
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//     <ScrollToTop>
+//       <Navbar />
+//       <Routes>
+//         {/* <Route path="/" element={<Navigate to="/home" />} /> */}
+//         <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//       </Routes>
+//       <Footer />
+
+//     </ScrollToTop>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Home from "../src/pages/home/Home";
 import About from "../src/pages/about/About";
 import Navbar from "../src/components/navbar/Navbar";
@@ -28,21 +56,27 @@ import Footer from "../src/components/footer/Footer";
 import ScrollToTop from "./dataHelper/ScrollToTop";
 import './App.css';
 
-function App() {
-  return (
-    <BrowserRouter>
-    <ScrollToTop>
-      <Navbar />
-      <Routes>
-        {/* <Route path="/" element={<Navigate to="/home" />} /> */}
-        <Route path="about" element={<About />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer />
+const Layout = () => (
+  <ScrollToTop>
+    <Navbar />
+    <Outlet />
+    <Footer />
+  </ScrollToTop>
+);
 
-    </ScrollToTop>
-    </BrowserRouter>
-  );
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "about", element: <About /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
   server: {
     host: true,
+    historyApiFallback: true,
   },
 })
