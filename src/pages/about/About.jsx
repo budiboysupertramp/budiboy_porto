095681a0618d6a08c59870f6051bc80c8b47fe20
budi <budiboy195@gmail.com>
2026-02-12 09:22:25 +0700

    add: add react lenis to smooth scrolling

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -4,97 +4,110 @@ import Me from "../../assets/img/belakang.jpg";
 import Stepper from "@keyvaluesystems/react-stepper";
 import DataHelper, { articleAbout, resume, budiSchool, budiSkill, organization } from "../../dataHelper/DataHelper";
 // import { initialStepsArr } from "../../dataHelper/DataHelper";
+import { Lenis } from 'lenis/react';
+
 
 const About = () => {
   return (
     <>
-      <section className="page-about relative w-full grid">
-        <img
-          src={Me}
-          alt=""
-          srcset=""
-          className="sticky overflow-hidden h-screen top-0 w-screen"
-        />
-        <div className="bx-about-page overflow-auto w-full flex flex-col md:pt-10 sm:pt-2">
-          <section className="header-about flex gap-5 flex-col">
-            <p className="md:text-3xl sm:text-xl font font-semibold">(About Me)</p>
-            <div className="col-head">
-              <h1 className="sm:text-5xl font-bold md:text-7xl lg:text-8xl xl:text-9xl">
-                Budiboy
-              </h1>
-              <h1 className="sm:text-5xl font-bold md:text-7xl lg:text-8xl xl:text-9xl">
-                Supertramp
-              </h1>
-            </div>
+      <Lenis
+       root
+        options={{
+          duration: 1.6,
+          smoothWheel: true,
+          smoothTouch: true,
+          easing: (t) => 1 - Math.pow(1 - t, 3),
 
-            <div className="bx-desc md:pt-3 lg:pt-10 lg:pt-10 sm:pt-0 w-full gap-4 flex flex-col">
-              {DataHelper &&
-                articleAbout
-                  .filter((description) => description.id)
-                  .map((description) => (
-                    <article key={description.id} className="text-justify font-semibold">
-                      <p>{description.desc}</p>
-                    </article>
-                  ))}
-            
-              <div className="divider-hor"></div>
-            </div>
+        }}
+      >
+        <section className="page-about relative w-full grid">
+          <img
+            src={Me}
+            alt=""
+            srcset=""
+            className="sticky overflow-hidden h-screen top-0 w-screen"
+          />
+          <div className="bx-about-page overflow-auto w-full flex flex-col md:pt-10 sm:pt-2">
+            <section className="header-about flex gap-5 flex-col">
+              <p className="md:text-3xl sm:text-xl font font-semibold">(About Me)</p>
+              <div className="col-head">
+                <h1 className="sm:text-5xl font-bold md:text-7xl lg:text-8xl xl:text-9xl">
+                  Budiboy
+                </h1>
+                <h1 className="sm:text-5xl font-bold md:text-7xl lg:text-8xl xl:text-9xl">
+                  Supertramp
+                </h1>
+              </div>
 
-            <div className="bx-cv">
-              <div className="container-cv w-full flex flex-col gap-8">
-                <div className="bx-content-cv w-full ">
-                  <h1 className="md:text-7xl sm:text-3xl font-bold">Work Experience</h1>
-                  <div className="w-full flex flex-col gap-5 md:mt-4 sm:mt-3">
-                    {DataHelper && resume.map((work, index)=> (
-                        <div key = {index} className="col-cv w-full md:pl-7 sm:pl-0" >
+              <div className="bx-desc md:pt-3 lg:pt-10 lg:pt-10 sm:pt-0 w-full gap-4 flex flex-col">
+                {DataHelper &&
+                  articleAbout
+                    .filter((description) => description.id)
+                    .map((description) => (
+                      <article key={description.id} className="text-justify font-semibold">
+                        <p>{description.desc}</p>
+                      </article>
+                    ))}
+
+                <div className="divider-hor"></div>
+              </div>
+
+              <div className="bx-cv">
+                <div className="container-cv w-full flex flex-col gap-8">
+                  <div className="bx-content-cv w-full ">
+                    <h1 className="md:text-7xl sm:text-3xl font-bold">Work Experience</h1>
+                    <div className="w-full flex flex-col gap-5 md:mt-4 sm:mt-3">
+                      {DataHelper && resume.map((work, index) => (
+                        <div key={index} className="col-cv w-full md:pl-7 sm:pl-0" >
                           <p className="col-cv-title sm:text-xl md:text-2xl">{work.title}</p>
                           <p className="col-cv-work sm:text-lg md:text-xl">{work.name}</p>
                           <p className="col-cv-date sm:text-base">{work.date}</p>
-                      </div>
-                    ))}
+                        </div>
+                      ))}
+                    </div>
                   </div>
-                </div>
 
-                <div className="bx-content-cv w-full">
-                  <h1 className="md:text-7xl sm:text-3xl font-bold">Education</h1>
-                  <div className="w-full flex flex-col gap-5 md:mt-4 sm:mt-3">
-                    {DataHelper && budiSchool.map((sch, index)=> (
-                        <div key = {index} className="col-cv w-full md:pl-7 sm:pl-0" >
+                  <div className="bx-content-cv w-full">
+                    <h1 className="md:text-7xl sm:text-3xl font-bold">Education</h1>
+                    <div className="w-full flex flex-col gap-5 md:mt-4 sm:mt-3">
+                      {DataHelper && budiSchool.map((sch, index) => (
+                        <div key={index} className="col-cv w-full md:pl-7 sm:pl-0" >
                           <p className="col-cv-title sm:text-xl md:text-2xl">{sch.title}</p>
                           <p className="col-cv-date sm:text-base">{sch.date}</p>
-                      </div>
-                    ))}
+                        </div>
+                      ))}
+                    </div>
                   </div>
-                </div>
 
-                <div className="bx-content-cv w-full">
-                  <h1 className="md:text-7xl sm:text-3xl font-bold">Skills</h1>
-                  <div className="w-full flex flex-col gap-5 md:mt-4 sm:mt-3">
-                    <div  className="col-cv w-full md:pl-7 sm:pl-4 grid md:grid-cols-4 sm:grid-cols-2 gap-2" >
-                    {DataHelper && budiSkill.map((skill, index)=> (
-                        <p key = {index} className="col-cv-work sm:text-lg md:text-xl list-item">{skill.title}</p>
-                    ))}
+                  <div className="bx-content-cv w-full">
+                    <h1 className="md:text-7xl sm:text-3xl font-bold">Skills</h1>
+                    <div className="w-full flex flex-col gap-5 md:mt-4 sm:mt-3">
+                      <div className="col-cv w-full md:pl-7 sm:pl-4 grid md:grid-cols-4 sm:grid-cols-2 gap-2" >
+                        {DataHelper && budiSkill.map((skill, index) => (
+                          <p key={index} className="col-cv-work sm:text-lg md:text-xl list-item">{skill.title}</p>
+                        ))}
                       </div>
+                    </div>
                   </div>
-                </div>
 
 
-                <div className="bx-content-cv w-full mb-5">
-                  <h1 className="md:text-7xl sm:text-3xl font-bold">Organization</h1>
-                  <div className="w-full flex flex-col gap-5 md:mt-4 sm:mt-3">
-                    {DataHelper && organization.map((org, index)=> (
-                        <div key = {index} className="col-cv w-full md:pl-7 sm:pl-0" >
-                           <p className="col-cv-title sm:text-xl md:text-2xl">{org.title}</p>
-                           <p className="col-cv-date sm:text-base">{org.date}</p>
-                      </div>
-                    ))}
+                  <div className="bx-content-cv w-full mb-5">
+                    <h1 className="md:text-7xl sm:text-3xl font-bold">Organization</h1>
+                    <div className="w-full flex flex-col gap-5 md:mt-4 sm:mt-3">
+                      {DataHelper && organization.map((org, index) => (
+                        <div key={index} className="col-cv w-full md:pl-7 sm:pl-0" >
+                          <p className="col-cv-title sm:text-xl md:text-2xl">{org.title}</p>
+                          <p className="col-cv-date sm:text-base">{org.date}</p>
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </section>
-        </div>
-      </section>
+            </section>
+          </div>
+        </section>
+      </Lenis>
     </>
   );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -11,102 +11,114 @@ import { Link } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
+import { Lenis } from 'lenis/react';
+
 const Home = () => {
-useEffect (() => {
-  AOS.init();
-})
+  useEffect(() => {
+    AOS.init();
+  })
 
 
   return (
     <>
+      <Lenis
+        root
+        options={{
+          duration: 1.6,
+          smoothWheel: true,
+          smoothTouch: true,
+          easing: (t) => 1 - Math.pow(1 - t, 3),
+
+        }}
+      >
         <section className="container-home w-full overflow-hidden">
           <div className="column-home w-ful flex flex-col">
             {/* <Navbar /> */}
             <Header />
-          <section className="ch-about w-full flex ">
-            <div className="row-content flex justify-between gap-3 sm:flex-col-reverse md:flex-col-reverse lg:flex-row ">
-              <h2 className='md:text-7xl sm:text-3xl '>
-              As a digital designer, I focus on producing top-notch and impactful digital experiences.</h2>
-              <div className="divider" data-aos="fade-up"  data-aos-duration="3000"></div>
-              <div className="row-right flex flex-col gap-2">
-                <span className='md:text-2xl sm:text-2xl'>(About Me)</span>
-                <div className="col-row-right flex gap-2">
+            <section className="ch-about w-full flex ">
+              <div className="row-content flex justify-between gap-3 sm:flex-col-reverse md:flex-col-reverse lg:flex-row ">
+                <h2 className='md:text-7xl sm:text-3xl '>
+                  As a digital designer, I focus on producing top-notch and impactful digital experiences.</h2>
+                <div className="divider" data-aos="fade-up" data-aos-duration="3000"></div>
+                <div className="row-right flex flex-col gap-2">
+                  <span className='md:text-2xl sm:text-2xl'>(About Me)</span>
+                  <div className="col-row-right flex gap-2">
 
-                  <Link to = "https://www.instagram.com/budiboy_supertramp/">
-                    <img src={Instagram} alt="instagram" srcset="" className=''/>
-                  </Link>
+                    <Link to="https://www.instagram.com/budiboy_supertramp/">
+                      <img src={Instagram} alt="instagram" srcset="" className='' />
+                    </Link>
 
-                  <Link>
-                    <img src={Twitter} alt="twitter" srcset="" className=''/>
-                  </Link>
+                    <Link>
+                      <img src={Twitter} alt="twitter" srcset="" className='' />
+                    </Link>
 
-                  <Link  to= "https://linkedin.com/in/setia-budi-345549232">
-                    <img src={Linkind} alt="linkind" srcset="" className=''/>
-                  </Link>
+                    <Link to="https://linkedin.com/in/setia-budi-345549232">
+                      <img src={Linkind} alt="linkind" srcset="" className='' />
+                    </Link>
+                  </div>
                 </div>
               </div>
-            </div>
-          </section>
+            </section>
 
-          <section className="bx-experience grid mt-8 sm:gap-4 md:gap-0 ">
-              <div className="content-experience " data-aos="fade-right"  data-aos-duration="1000">
-                  <h1 className='sm:text-3xl md:text-3xl'>
-                    (What I do)
-                  </h1>
+            <section className="bx-experience grid mt-8 sm:gap-4 md:gap-0 ">
+              <div className="content-experience " data-aos="fade-right" data-aos-duration="1000">
+                <h1 className='sm:text-3xl md:text-3xl'>
+                  (What I do)
+                </h1>
               </div>
-              <div className="content-experience " data-aos="fade-left"  data-aos-duration="900">
-                 <article className='md:text-left sm:text-justify' >
+              <div className="content-experience " data-aos="fade-left" data-aos-duration="900">
+                <article className='md:text-left sm:text-justify' >
                   <h1 className='sm:text-2xl md:text-3xl'>
                     UI Designer
                   </h1>
                   <p>
-                  Design dashboards and user interfaces of information systems for industrial and institutional use. 
-                  government.
+                    Design dashboards and user interfaces of information systems for industrial and institutional use.
+                    government.
                   </p>
-                 </article>
+                </article>
               </div>
-              <div className="content-experience " data-aos="fade-right"  data-aos-duration="1000">
-                 <article className='md:text-left sm:text-justify' >
+              <div className="content-experience " data-aos="fade-right" data-aos-duration="1000">
+                <article className='md:text-left sm:text-justify' >
                   <h1 className='sm:text-2xl md:text-3xl'>
                     Web Designer
-                    </h1>
-                    <p>
-                    Transformed comprehensive layouts into well-functioning web and mobile screen displays 
+                  </h1>
+                  <p>
+                    Transformed comprehensive layouts into well-functioning web and mobile screen displays
                     using HTML, CSS, JavaScript, Jquery libraries.
-                    </p>
-                 </article>
+                  </p>
+                </article>
               </div>
-              <div className="content-experience " data-aos="fade-left"  data-aos-duration="2000">
-                  <article className='md:text-left sm:text-justify'>
-                    <h1 className='sm:text-2xl md:text-3xl'>
+              <div className="content-experience " data-aos="fade-left" data-aos-duration="2000">
+                <article className='md:text-left sm:text-justify'>
+                  <h1 className='sm:text-2xl md:text-3xl'>
                     UX Designer
-                    </h1>
-                    <p>
+                  </h1>
+                  <p>
                     Work closely with developers, project managers, in the design, implementation and handover process.
-                    </p>
-                  </article>
+                  </p>
+                </article>
               </div>
-              <div className="content-experience " data-aos="fade-right"  data-aos-duration="2500">
-                  <article className='md:text-left sm:text-justify'>
-                    <h1 className='sm:text-2xl md:text-3xl'>
+              <div className="content-experience " data-aos="fade-right" data-aos-duration="2500">
+                <article className='md:text-left sm:text-justify'>
+                  <h1 className='sm:text-2xl md:text-3xl'>
                     Responsive design
-                    </h1>
-                    <p>
+                  </h1>
+                  <p>
                     provides comfort to users in using the service
-                    </p>
-                  </article>
+                  </p>
+                </article>
               </div>
             </section>
           </div>
 
-            <div className="">
-              <Banner />
-            </div>
-            <div className="column-home overflow-hidden">
-              <Project />
-            </div>
+          <div className="">
+            <Banner />
+          </div>
+          <div className="column-home overflow-hidden">
+            <Project />
+          </div>
         </section>
-    
+      </Lenis>
     </>
   )
 }
